ba605c83dc53653137f5f494c7d283305228dfb3
ImanMontajabi <iman.montajabi@gmail.com>
2024-05-01 14:01:37 +0330

    added track_detail and apparently works everything well

--- stats/spotify_stats_normal.py
+++ stats/spotify_stats_normal.py
@@ -6,11 +6,18 @@ from spotify_auth import auth_header
 from init import proxies, artist_page, base_url, cur, con
 
 
-def album_ids():
+def albums_details():
     # album_id is final output of this function
     cur.execute('''
-        CREATE TABLE IF NOT EXISTS albums_id (
-            album_id TEXT PRIMARY KEY
+        CREATE TABLE IF NOT EXISTS albums_details (
+            album_id TEXT PRIMARY KEY,
+            album_name TEXT,
+            total_tracks TEXT,
+            release_date TEXT,
+            album_type TEXT,
+            artist_names TEXT, 
+            artist_ids TEXT,
+            cover TEXT
         )
     ''')
 
@@ -36,10 +43,40 @@ def album_ids():
     id_list_for_query: List[Tuple] = []
     for album in resp_json:
         for i in range(0, len(album['items'])):
-            id_list_for_query.append((album['items'][i]['id'],))
+            album_id = album['items'][i]['id']
+            album_name = album['items'][i]['name']
+            total_tracks = album['items'][i]['total_tracks']
+            release_date = album['items'][i]['release_date']
+            album_type = album['items'][i]['album_type']
+            cover = album['items'][i]['images'][0]['url']
+            artist_names: List['str'] = []
+            artist_ids: List['str'] = []
+            for artist in album['items'][i]['artists']:
+                artist_names.append(artist['name'])
+                artist_ids.append(artist['id'])
+
+            id_list_for_query.append(
+                (album_id,
+                 album_name,
+                 total_tracks,
+                 release_date,
+                 album_type,
+                 ','.join(artist_names),
+                 ','.join(artist_ids),
+                 cover
+                 )
+            )
 
     cur.executemany('''
-        INSERT OR IGNORE INTO albums_id (album_id) VALUES (?)
+        INSERT OR IGNORE INTO albums_details (
+            album_id, 
+            album_name, 
+            total_tracks, 
+            release_date, 
+            album_type, 
+            artist_names, 
+            artist_ids,
+            cover) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         ''', id_list_for_query)
     con.commit()
 
@@ -48,28 +85,35 @@ def all_tracks():
     # creating table tracks_id to save track_id tracks_name and release_date
     cur.execute('''
         CREATE TABLE IF NOT EXISTS tracks_id (
-        track_id TEXT PRIMARY KEY,
-        track_name TEXT,
-        release_date TEXT
+            track_id TEXT PRIMARY KEY,
+            track_name TEXT,
+            release_date TEXT
         )
     ''')
 
     album_ids_list: List[str] = []
-    albums_jsons: List[Dict] = []
+    albums_json: List[Dict] = []
     id_list_for_query: List[Tuple] = []
     # reading albums ids from database
-    for row in cur.execute('''SELECT * FROM albums_id'''):
+    for row in cur.execute('''SELECT * FROM albums_details'''):
         album_ids_list.append(row[0])
 
     for i in range(0, len(album_ids_list), 20):
         album_id_20_string = ','.join(album_ids_list[i: i + 20])
         url = f'{base_url}/albums?ids={album_id_20_string}'
         response = requests.get(url, headers=auth_header, proxies=proxies)
-        albums_jsons.append(response.json()['albums'])
+        albums_json.append(response.json()['albums'])
 
-    for album_20 in albums_jsons:
-        for album in album_20:
+    for twenty_albums in albums_json:
+        for album in twenty_albums:
             release_date = album['release_date']
+            if album['tracks']['next'] is not None:
+                next_tracks_url = album['tracks']['next']
+                next_response = requests.get(
+                    next_tracks_url,
+                    headers=auth_header,
+                    proxies=proxies)
+                albums_json.append(next_response.json()['albums'])
             for track in range(0, len(album['tracks']['items'])):
                 name = album['tracks']['items'][track]['name']
                 track_id = album['tracks']['items'][track]['id']
@@ -81,10 +125,76 @@ def all_tracks():
     con.commit()
 
 
+def track_details():
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS track_details (
+            track_id TEXT PRIMARY KEY,
+            track_name TEXT,
+            release_date TEXT,
+            duration_ms INTEGER,
+            popularity INTEGER,
+            cover_album TEXT,
+            artists_names TEXT,
+            artists_ids TEXT
+        )
+    ''')
+
+    track_id_list: List[str] = []
+    tracks_json: List[Dict] = []
+    for row in cur.execute('''SELECT * FROM tracks_id'''):
+        track_id_list.append(row[0])
+
+    for i in range(0, len(track_id_list), 50):
+        track_id_50_string = ','.join(track_id_list[i: i + 50])
+        url = f'{base_url}/tracks?ids={track_id_50_string}'
+        response = requests.get(url, headers=auth_header, proxies=proxies)
+        tracks_json.append(response.json()['tracks'])
+
+    track_details_for_query: List[Tuple] = []
+    for fifty_track in tracks_json:
+        for track in fifty_track:
+            track_id = track['id']
+            track_name = track['name']
+            release_date = track['album']['release_date']
+            duration_ms = track['duration_ms']
+            popularity = track['popularity']
+            cover_album = track['album']['images'][0]['url']
+            artists_names: List[str] = []
+            artists_ids: List[str] = []
+            for artist_name in track['artists']:
+                artists_names.append(artist_name['name'])
+                artists_ids.append(artist_name['id'])
+
+            track_details_for_query.append(
+                (track_id,
+                 track_name,
+                 release_date,
+                 duration_ms,
+                 popularity,
+                 cover_album,
+                 ','.join(artists_names),
+                 ','.join(artists_ids)
+                 )
+            )
+
+    cur.executemany('''
+        INSERT OR IGNORE INTO track_details (
+            track_id,
+            track_name,
+            release_date,
+            duration_ms,
+            popularity,
+            cover_album,
+            artists_names,
+            artists_ids) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    ''', track_details_for_query)
+    con.commit()
+
 
 def main():
-    # album_ids()
+    albums_details()
     all_tracks()
+    track_details()
 
 
 if __name__ == '__main__':
