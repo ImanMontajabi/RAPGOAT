6cf118acae515c157e79d6c0b233bf8d12f72b8c
ImanMontajabi <iman.montajabi@gmail.com>
2024-05-18 03:14:32 +0330

    test soundcloud

--- RAPGOAT/main.py
+++ RAPGOAT/main.py
@@ -4,21 +4,21 @@ import tables_to_csv
 
 
 if __name__ == '__main__':
-    print('- spotify is started...')
-    try:
-        spotify_stats.main()
-    except Exception as e:
-        print(f'Updating process spotify in main.py encountered an error: {e}')
-    else:
-        print('Spotify successfully updated')
-
+    # print('- spotify is started...')
     # try:
-    #     soundcloud_stats.main()
+    #     spotify_stats.main()
     # except Exception as e:
-    #     print(f'Updating process soundcloud'
-    #           f' in main.py encountered an error: {e}')
+    #     print(f'Updating process spotify in main.py encountered an error: {e}')
     # else:
-    #     print('Soundcloud successfully updated')
+    #     print('Spotify successfully updated')
+
+    try:
+        soundcloud_stats.main()
+    except Exception as e:
+        print(f'Updating process soundcloud'
+              f' in main.py encountered an error: {e}')
+    else:
+        print('Soundcloud successfully updated')
     #
     # try:
     #     tables_to_csv.main()
