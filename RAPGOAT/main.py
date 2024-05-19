edf4a8a20f81b40575d2c0fdf5a6d2926e8d9a39
ImanMontajabi <iman.montajabi@gmail.com>
2024-05-19 18:20:28 +0330

    spotify run

--- RAPGOAT/main.py
+++ RAPGOAT/main.py
@@ -1,25 +1,25 @@
-# import spotify_stats
-import soundcloud_stats
+import spotify_stats
+# import soundcloud_stats
 import tables_to_csv
 
 
 if __name__ == '__main__':
-    # print('- spotify is started...')
-    # try:
-    #     spotify_stats.main()
-    # except Exception as e:
-    #     print(f'Updating process spotify in main.py encountered an error: {e}')
-    # else:
-    #     print('Spotify successfully updated')
-
+    print('- spotify is started...')
     try:
-        print('- soundcloud is started...')
-        soundcloud_stats.main()
+        spotify_stats.main()
     except Exception as e:
-        print(f'Updating process soundcloud'
-              f' in main.py encountered an error: {e}')
+        print(f'Updating process spotify in main.py encountered an error: {e}')
     else:
-        print('Soundcloud successfully updated')
+        print('Spotify successfully updated')
+
+    # try:
+    #     print('- soundcloud is started...')
+    #     soundcloud_stats.main()
+    # except Exception as e:
+    #     print(f'Updating process soundcloud'
+    #           f' in main.py encountered an error: {e}')
+    # else:
+    #     print('Soundcloud successfully updated')
 
     try:
         print('- convertor is started...')
--- RAPGOAT/tables_to_csv.py
+++ RAPGOAT/tables_to_csv.py
@@ -4,101 +4,116 @@ import csv
 import sqlite3
 
 
-
-# TODO: set null for not exist data
+"""
+This script converts SQLite database tables to CSV files.
+The database file is assumed to be named 'RapGoat.db' and located in the
+current working directory. The CSV files will be saved in a 'CSV' subdirectory
+within the current working directory."""
 
 
 def get_database_dir() -> str:
+    """
+    Get the directory of the database file.
+    :return:
+        str: The absolute path to the database file."""
+
     path = os.getcwd()
     database_dir = path + '/RapGoat.db'
     return database_dir
 
 
-def check_database(db_dir: str):
+def check_database(db_dir: str) -> None:
+    """
+    Check if the database file exists in the specified directory.
+    :arg:
+        db_dir (str): The path to the database file.
+    :raise:
+        SystemExit: If the database file does not exist."""
     if not os.path.isfile(db_dir):
         print('There is no such database in this directory!')
         exit(1)
 
 
-def fetch_all(db_dir: str):
+def convertor(database_dir: str) -> None:
+    """
+    Convert the database tables to CSV files.
+    :arg:
+        database_dir (str): The path to the database file.
+    :raise:
+        SystemExit: If database connection or data export fails."""
+
+    check_database(database_dir)
+
     try:
-        con = sqlite3.connect(db_dir)
+        con = sqlite3.connect(database_dir)
         cur = con.cursor()
     except sqlite3.DatabaseError as e:
-        print(f'Data base connection was unsuccessful: {e}')
-        exit(1)
-
-    # try:
-    #     cur.execute('''
-    #             SELECT track_id, track_name, release_date, duration_ms,
-    #             popularity, cover_album, artists_names, artists_ids
-    #             FROM track_details
-    #         ''')
-    #     all_data = cur.fetchall()
-    # except sqlite3.DatabaseError as e:
-    #     print(f'Data Export was unsuccessful: {e}')
-    #     exit(1)
-    # else:
-    #     table_headers: list[str] = [desc[0] for desc in cur.description]
-
-    try:
-        cur.execute('''
-                SELECT page_url, page_name, followers, followings, 
-                tracks, avatar_url
-                FROM soundcloud_artist
-            ''')
-        all_data = cur.fetchall()
-    except sqlite3.DatabaseError as e:
-        print(f'Data Export was unsuccessful: {e}')
+        print(f'Data base connection was unsuccessful > {e}')
         exit(1)
     else:
-        table_headers: list[str] = [desc[0] for desc in cur.description]
+        # table_list includes all table name that includes in our database
+        table_list = get_table_names(cur)
 
-    this_dir: str = os.getcwd()
-    if not os.path.exists(this_dir + '/CSV'):
-        csv_path: str = os.path.join(this_dir, 'CSV')
-        os.mkdir(csv_path)
-    csv_file_dir: str = this_dir + '/CSV/soundcloud_artist.csv'
-    with open(csv_file_dir, 'w', newline='') as csvfile:
-        writer = csv.writer(
-            csvfile, delimiter=',', lineterminator='\r\n',
-            quoting=csv.QUOTE_ALL, escapechar='\\')
-        writer.writerow(table_headers)
-        writer.writerows(all_data)
+    for table in table_list:
+        table_name = table[0]
+        try:
+            cur.execute(f'''
+                    SELECT * FROM {table_name}
+                ''')
+            all_data = cur.fetchall()
+        except sqlite3.DatabaseError as e:
+            print(f'Data Export from {table_name} was unsuccessful > {e}')
+            exit(1)
+        else:
+            '''According to PEP 249, this read-only attribute is a sequence of 
+            7-items sequences. Each of these sequences contains information
+            describing one result column. Here we need just "name". 
+            name,type_code,display_size,internal_size,precision,scale,null_ok
+            '''
+            table_headers: list[str] = [desc[0] for desc in cur.description]
 
-    try:
-        cur.execute('''
-                SELECT track_url, page_name, track_title, plays, 
-                likes, comments, upload_datetime, cover_url
-                FROM soundcloud_tracks
-            ''')
-        all_data = cur.fetchall()
-    except sqlite3.DatabaseError as e:
-        print(f'Data Export was unsuccessful: {e}')
-        exit(1)
-    else:
-        table_headers: list[str] = [desc[0] for desc in cur.description]
+        '''Creates CSV folder in this place, if it not exists'''
+        this_dir: str = os.getcwd()
+        if not os.path.exists(this_dir + '/CSV'):
+            print(f'Such a directory did not exist.\nNow is created...')
+            csv_dir: str = os.path.join(this_dir, 'CSV')
+            os.mkdir(csv_dir)
 
-    this_dir: str = os.getcwd()
-    if not os.path.exists(this_dir + '/CSV'):
-        csv_path: str = os.path.join(this_dir, 'CSV')
-        os.mkdir(csv_path)
-    csv_file_dir: str = this_dir + '/CSV/soundcloud_tracks.csv'
-    with open(csv_file_dir, 'w', newline='') as csvfile:
-        writer = csv.writer(
-            csvfile, delimiter=',', lineterminator='\r\n',
-            quoting=csv.QUOTE_ALL, escapechar='\\')
-        writer.writerow(table_headers)
-        writer.writerows(all_data)
+        '''Defines name of .csv file and opens it to write data in it'''
+        csv_file_dir: str = this_dir + f'/CSV/{table_name}.csv'
+        with open(csv_file_dir, 'w', newline='') as csvfile:
+            writer = csv.writer(
+                csvfile, delimiter=',', lineterminator='\r\n',
+                quoting=csv.QUOTE_ALL, escapechar='\\')
+            writer.writerow(table_headers)
+            writer.writerows(all_data)
 
     print('Data export was successful')
     con.close()
 
 
+def get_table_names(cur: sqlite3.Cursor) -> list:
+    """
+    Retrieve the list of table names in the database.
+    :arg:
+        cur (sqlite3.Cursor): The database cursor.
+    :return:
+        list: A list of table names.
+    :raise:
+        SystemExit: If fetching table names fails."""
+
+    try:
+        cur.execute('''
+            SELECT name FROM sqlite_master WHERE type='table';
+        ''')
+    except sqlite3.Error as e:
+        print(f'get_table_name > failed to retrieve table names > {e}')
+        exit(1)
+    else:
+        return cur.fetchall()
+
+
 def main():
-    # get database directory
-    db_dir: str = get_database_dir()
-    # checks if database exists
-    check_database(db_dir)
-    # create connection to database and fetch all data and calls save_to_csv()
-    fetch_all(db_dir)
+    """The main function to execute the database to CSV conversion process."""
+    database_dir = get_database_dir()
+    convertor(database_dir)
